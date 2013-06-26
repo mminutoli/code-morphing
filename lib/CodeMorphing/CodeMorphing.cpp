432258695cc297ab5a6e0f1a672adcd4ad91cc61
Marco Minutoli <mminutoli@gmail.com>
2013-06-26 17:53:57 +0200

    [cmp] Removed the choice vectors.
    We agreed that the randomization should happen before every instruction
    replaced insted of using always the same alternatie for every invocation
    of the function.
    
    For this reason we don't need any more a choice vector but we just need
    to call the randmize function with the right parameter.

--- lib/CodeMorphing/CodeMorphing.cpp
+++ lib/CodeMorphing/CodeMorphing.cpp
@@ -51,7 +51,6 @@ namespace {
 typedef std::pair<Instruction *, std::vector<BasicBlock *> > replacement;
 }
 
-static bool InsertChoiceVector(Function &, Pass *);
 static bool
 BuildReplacementList(Function & , std::vector<replacement> &);
 static bool
@@ -63,8 +62,7 @@ bool CodeMorphing::runOnFunction(Function & F)
   result |= BuildReplacementList(F, replacementList);
   if (result)
   {
-    InsertChoiceVector(F, this);
-    InsertAlternativeBlocks(F, replacementList, this);
+    result |= InsertAlternativeBlocks(F, replacementList, this);
   }
   return result;
 }
@@ -87,117 +85,6 @@ INITIALIZE_PASS(CodeMorphing, "static-morphing",
                 "Static Code Morphing Pass",
                 false,
                 false)
-
-
-static bool
-InsertChoiceVector(Function & F, Pass * P)
-{
-  // Build basic block structure for the initialization loop.  The
-  // entry block is splitted in a way that will produce the following
-  // blocks:
-  //   1. A new empty entry block (entry)
-  //   2. A block hosting the loop condition (loopCondition)
-  //   3. A block hosting the loop body (loopBody)
-  //   4. The old entry block reached at loop exit (oldEntry)
-  BasicBlock * entry = F.begin();
-  BasicBlock * loopCondition =
-      SplitBlock(entry, entry->begin(), P);
-  loopCondition->setName("loopCondition");
-
-  BasicBlock * oldEntry = SplitEdge(entry, loopCondition, P);
-  oldEntry->setName("oldEntry");
-  BasicBlock * loopBody = SplitBlock(loopCondition, loopCondition->begin(), P);
-  loopBody->setName("loopBody");
-
-  // Fill the new entry block with instruction that:
-  //   1. allocate space for the choice vector
-  //   2. allocate space for the index variable and initialize it.
-  Type * Int32Ty = Type::getInt32Ty(F.getContext());
-  ArrayType * Int32ArrayTy =
-      ArrayType::get(Int32Ty, LastInstructionTy);
-  AllocaInst * regRegChoiceVector =
-      new AllocaInst(Int32ArrayTy, "regreg.choice.vector", entry->getTerminator());
-  regRegChoiceVector->setAlignment(4);
-  AllocaInst * regConstChoiceVector =
-      new AllocaInst(Int32ArrayTy, "regconst.choice.vector", entry->getTerminator());
-  regConstChoiceVector->setAlignment(4);
-
-
-  AllocaInst * index =
-      new AllocaInst(Int32Ty, "idx.vector", entry->getTerminator());
-  index->setAlignment(4);
-  Constant * lastInstrConstant = ConstantInt::get(Int32Ty, LastInstructionTy);
-  (void)new StoreInst(lastInstrConstant, index, entry->getTerminator());
-
-  // Fill the loopCondition block with instruction that:
-  //   1. decrese the index
-  //   2. test the exit condition
-  //   3. jump to the appropriate block
-  Instruction * originalJump = loopCondition->getTerminator();
-
-  LoadInst * loadIdx = new LoadInst(index, "", originalJump);
-  Constant * minusOne = ConstantInt::get(Int32Ty, -1, true);
-  BinaryOperator * decInst =
-      BinaryOperator::CreateAdd(loadIdx, minusOne, ".dec", originalJump);
-  StoreInst * newIdxValue = new StoreInst(decInst, index, originalJump);
-
-  Constant * zero = ConstantInt::get(Int32Ty, 0, true);
-  ICmpInst * loopTest =
-      new ICmpInst(originalJump, ICmpInst::ICMP_NE, loadIdx, zero);
-
-  BranchInst * conditionalBranch =
-      BranchInst::Create(loopBody, oldEntry, loopTest);
-  ReplaceInstWithInst(originalJump, conditionalBranch);
-
-  // Fill the loopBody block with instruction that:
-  //   1. retrieve index
-  //   2. retrieve the value to be passed to randomize
-  //   3. call randomize
-  //   4. store the result in the choice vector
-  //   5. jump back to the loopCondition block
-  const ValueSymbolTable & VST = F.getParent()->getValueSymbolTable();
-  Value * randomize = VST.lookup("randomize");
-  Value * regRegAlternativesVector = VST.lookup("RegRegAlternativeNumber");
-  Value * regConstAlternativesVector = VST.lookup("RegConstAlternativeNumber");
-
-  originalJump = loopBody->getTerminator();
-  loadIdx = new LoadInst(index, "", originalJump);
-
-  std::vector<Value *> indexes;
-  indexes.push_back(zero);
-  indexes.push_back(loadIdx);
-  GetElementPtrInst * regRegAlternativesVecElemPtr =
-      GetElementPtrInst::Create(regRegAlternativesVector, indexes,
-                                "", originalJump);
-  regRegAlternativesVecElemPtr->setIsInBounds(true);
-  LoadInst * regRegLoadArg = new LoadInst(regRegAlternativesVecElemPtr,
-                                          "", originalJump);
-  GetElementPtrInst * regConstAlternativesVecElemPtr =
-      GetElementPtrInst::Create(regConstAlternativesVector, indexes,
-                                "", originalJump);
-  regConstAlternativesVecElemPtr->setIsInBounds(true);
-  LoadInst * regConstLoadArg = new LoadInst(regConstAlternativesVecElemPtr,
-                                            "", originalJump);
-
-  CallInst * regRegRandomizeCall =
-      CallInst::Create(randomize, regRegLoadArg, "", originalJump);
-  CallInst * regConstRandomizeCall =
-      CallInst::Create(randomize, regConstLoadArg, "", originalJump);
-
-  GetElementPtrInst * regRegChoiceVecElemPtr =
-      GetElementPtrInst::Create(regRegChoiceVector, indexes, "", originalJump);
-  regRegChoiceVecElemPtr->setIsInBounds(true);
-  newIdxValue = new StoreInst(regRegRandomizeCall, regRegChoiceVecElemPtr, originalJump);
-  GetElementPtrInst * regConstChoiceVecElemPtr =
-      GetElementPtrInst::Create(regConstChoiceVector, indexes, "", originalJump);
-  regConstChoiceVecElemPtr->setIsInBounds(true);
-  newIdxValue = new StoreInst(regConstRandomizeCall, regConstChoiceVecElemPtr, originalJump);
-
-  BranchInst * jumpBackToCond = BranchInst::Create(loopCondition);
-  ReplaceInstWithInst(originalJump, jumpBackToCond);
-
-  return true;
-}
 
 
 static bool
@@ -226,14 +113,14 @@ BuildReplacementList(Function & F, std::vector<replacement> & V)
 }
 
 
-std::string choiceVectorName(Instruction * i)
+static std::string alternativesVectorName(Instruction * i)
 {
   Value * firstOperand = i->getOperand(0);
   Value * secondOperand = i->getOperand(1);
 
   if (isa<Constant>(firstOperand) || isa<Constant>(secondOperand))
-    return "regconst.choice.vector";
-  return "regreg.choice.vector";
+    return "RegConstAlternativeNumber";
+  return "RegRegAlternativeNumber";
 }
 
 
@@ -256,18 +143,27 @@ InsertAlternativeBlocks(Function & F, std::vector<replacement> & V, Pass * P)
     lowerBlock->setName("");
 
     IntegerType * Int32Ty = Type::getInt32Ty(F.getContext());
-    Value * choiceVector = F.getValueSymbolTable().lookup(choiceVectorName(i));
-    assert(choiceVector && "choice.vector not found!");
+    Value * alternativesVector =
+        F.getParent()->getValueSymbolTable().lookup(alternativesVectorName(i));
+    assert(alternativesVector && "Alternatives vector not found!");
+
     ConstantInt * zero = ConstantInt::get(Int32Ty, 0);
     ConstantInt * index = ConstantInt::get(Int32Ty, getInstTy(i));
     std::vector<Value *> indexes;
     indexes.push_back(zero);
     indexes.push_back(index);
-    GetElementPtrInst * choicePtr =
-        GetElementPtrInst::Create(choiceVector, indexes, "",
+
+    GetElementPtrInst * alternativesPtr =
+        GetElementPtrInst::Create(alternativesVector, indexes, "",
                                   upperBlock->getTerminator());
-    LoadInst * choice =
-        new LoadInst(choicePtr, "", upperBlock->getTerminator());
+    LoadInst * numberOfAlternatives =
+        new LoadInst(alternativesPtr, "", upperBlock->getTerminator());
+
+    const ValueSymbolTable & VST = F.getParent()->getValueSymbolTable();
+    Value * randomize = VST.lookup("randomize");
+    CallInst * choice =
+        CallInst::Create(randomize, numberOfAlternatives,
+                         "", upperBlock->getTerminator());
     SwitchInst * test = SwitchInst::Create(choice, originalBlock, 0);
     ReplaceInstWithInst(upperBlock->getTerminator(), test);
 
